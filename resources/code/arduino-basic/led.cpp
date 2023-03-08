9ac808039813601b2b6f671a9772a1bdcd6de3f7
Syan Delbart <syan.delbart@hotmail.com>
2023-03-08 14:02:25 +0100

    Changed folder for arduino-basic

---
+++ resources/code/arduino-basic/led.cpp
@@ -0,0 +1,24 @@
+// Oefening 1 - LED
+
+#include <Arduino.h>
+// Definieer pinnummers
+const int LED_PIN = 3;
+
+void setup()
+{
+    Serial.begin(9600);
+
+    // Definiëer INPUT & OUTPUT pinnen
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop()
+{
+
+    // Zet LED aan
+    digitalWrite(LED_PIN, HIGH);
+    // Wacht 1000ms (=1 seconde)
+    delay(1000);
+    // Zet LED uit
+    digitalWrite(LED_PIN, LOW);
+}
